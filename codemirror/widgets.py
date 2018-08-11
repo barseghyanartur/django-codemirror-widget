c58fd34a37d1d3156db24555bd6a17fb6d9fb93e
Marco Bontempi <marco@travelbird.nl>
2018-08-12 00:35:01 +0200

    Pass kwargs to super

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -165,7 +165,8 @@ class CodeMirrorTextarea(forms.Textarea):
             js_var_bit = 'var %s = ' % (self.js_var_format % name)
         else:
             js_var_bit = ''
-        output = [super(CodeMirrorTextarea, self).render(name, value, attrs),
+        output = [super(CodeMirrorTextarea, self).render(name, value, attrs,
+                                                         **kwargs),
             '<script type="text/javascript">%sCodeMirror.fromTextArea(document.getElementById(%s), %s);</script>' %
                 (js_var_bit, '"id_%s"' % name, self.option_json)]
         return mark_safe('\n'.join(output))
